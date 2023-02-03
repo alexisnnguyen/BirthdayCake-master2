f35e046fe3ba352533acf368b9969ed2793c0e05
Alexis Nguyen <alexisnguyen18@icloud.com>
2023-02-02 17:35:51 -0800

    V5 Added function to close app on goodbye.

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     public void goodbye(View button) {
         //System.out.println("Goodbye");
         Log.i("button","Goodbye");
+        finishAffinity();
     }
 }
